531d69c7bf09a604682a8b8620351adf5fe8d3fb
Ramya <ramyaneela36@gmail.com>
2023-09-10 00:58:27 -0400

    review file code added

--- reviews.py
+++ reviews.py
@@ -1,2 +1,17 @@
-# add your code here
+import pandas as pd
+
+# Read the CSV data
+df = pd.read_csv('data/winemag-data-130k-v2.csv.zip')
+
+# Group the data by 'country' and calculate the number of reviews and average points
+summary_df = df.groupby('country').agg({'country': 'count', 'points': 'mean'}).rename(columns={'country': 'count', 'points': 'average_points'})
+summary_df['average_points'] = summary_df['average_points'].round(1)
+# Reset the index to make 'country' a regular column
+summary_df.reset_index(inplace=True)
+
+# Save the summary data to a new CSV file
+summary_df.to_csv('data/reviews-per-country.csv', sep='\t', index=False)
+
+print("Summary data has been saved to 'data/reviews-per-country.csv'")
+
 
